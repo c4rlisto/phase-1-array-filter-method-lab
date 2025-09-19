e3c9eb3302d0eba411881d3009a9073487a94b97
Carl Kiprono <carlkiprono00@gmail.com>
2025-09-19 13:56:00 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+function findMatching(drivers, name) {
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === name.toLowerCase()
+  );
+}
+
+function fuzzyMatch(drivers, letters) {
+  return drivers.filter((driver) =>
+    driver.toLowerCase().startsWith(letters.toLowerCase())
+  );
+}
+
+function matchName(drivers, name) {
+  return drivers.filter((driver) => driver.name === name);
+}
+
+// 👇 Export all three so tests can see them
+module.exports = {
+  findMatching,
+  fuzzyMatch,
+  matchName,
+};
